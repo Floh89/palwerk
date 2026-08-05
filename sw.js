3a70d1f5277c8933ec096b71a94854f2a916e21a
Floh89 <97030217+Floh89@users.noreply.github.com>
2026-08-05 20:49:01 +0200

    Refresh offline cache for catalog package 3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='palwerk-shell-v5';
+const CACHE='palwerk-shell-v6';
 const ASSETS=['./','./index.html','./src/styles.css','./src/app.js','./src/storage.js','./src/catalog.js','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
