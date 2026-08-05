1bd2c539ca488cda7d5f80a3355236e3cd118476
Floh89 <97030217+Floh89@users.noreply.github.com>
2026-08-05 23:40:02 +0200

    Add single controlled application bootstrap

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './catalog-pack-5.js';
+import './catalog-pack-6.js';
+import './catalog-pack-7.js';
+import './generated-core.js';
+import './generated-partner-data.js';
+import './combat-data.js';
+
+import './app.js';
+import './data-enrichment-ui.js';
+import './pal-images-ui.js';
+import { installOptimizerUI } from './optimizer/ui.js';
+
+installOptimizerUI();
+
+window.PALWERK_RUNTIME = Object.freeze({
+  bootstrap: 'src/main.js',
+  optimizer: 'src/optimizer/engine.js',
+  architectureVersion: '1.0.0'
+});
