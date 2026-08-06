abb4b9a0c8331451204425d3b5c837bafe30fdcc
Floh89 <97030217+Floh89@users.noreply.github.com>
2026-08-06 21:33:00 +0200

    Install phase 4 synergy laboratory

--- src/main.js
+++ src/main.js
@@ -9,11 +9,14 @@ import './app.js';
 import './data-enrichment-ui.js';
 import './pal-images-ui.js';
 import { installOptimizerUI } from './optimizer/ui.js';
+import { installSynergyUI } from './optimizer/synergy-ui.js';
 
 installOptimizerUI();
+installSynergyUI();
 
 window.PALWERK_RUNTIME = Object.freeze({
   bootstrap: 'src/main.js',
   optimizer: 'src/optimizer/engine.js',
-  architectureVersion: '1.0.0'
+  synergy: 'src/optimizer/synergy.js',
+  architectureVersion: '1.1.0'
 });
