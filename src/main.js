59f2873dd6cdaafe4685588f3265224f1b997345
Floh89 <97030217+Floh89@users.noreply.github.com>
2026-08-07 07:04:48 +0200

    Expose canonical passive data

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './generated-core.js';
 import './generated-partner-data.js';
 import './combat-data.js';
 import { CANONICAL_PALS, canonicalDataReport } from './data/canonical-pals.js';
+import { PASSIVES, passiveDataReport } from './data/passives.js';
 import { COMBAT_GRAPH, graphQualitySummary } from './knowledge/combat-graph.js';
 
 import './app.js';
@@ -17,14 +18,18 @@ installOptimizerUI();
 installSynergyUI();
 
 window.PALWERK_CANONICAL_PALS = CANONICAL_PALS;
+window.PALWERK_PASSIVES = PASSIVES;
 window.PALWERK_COMBAT_GRAPH = COMBAT_GRAPH;
 window.PALWERK_RUNTIME = Object.freeze({
   bootstrap: 'src/main.js',
   optimizer: 'src/optimizer/engine.js',
+  passiveOptimizer: 'src/optimizer/passive-builds.js',
   canonicalData: 'src/data/canonical-pals.js',
   canonicalReport: canonicalDataReport(CANONICAL_PALS),
+  passiveData: 'src/data/passives.js',
+  passiveReport: passiveDataReport(PASSIVES),
   knowledgeGraph: 'src/knowledge/combat-graph.js',
   knowledgeQuality: graphQualitySummary(COMBAT_GRAPH),
   synergy: 'src/optimizer/synergy.js',
-  architectureVersion: '1.3.0'
+  architectureVersion: '1.4.0'
 });
